897d1097d4ad8bd5e5fdd10b8477b34d3d8e102f
lollo1297 <lorenzo97.nardi@gmail.com>
2020-11-15 17:22:27 +0100

    First changes
    Added main menu scene, started working on menu interaction

---
+++ Kovid19Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    //Closes game whenever clicked
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
