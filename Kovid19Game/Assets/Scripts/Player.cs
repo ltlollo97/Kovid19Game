0c3acc1b8f09f51aaaba54024532b79296fdda24
lollo1297 <lorenzo97.nardi@gmail.com>
2020-11-16 12:18:57 +0100

    2nd update
    Added prototype of first level and basic movement (left-right)

---
+++ Kovid19Game/Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int playerSpeed = 10;
+    public bool facingRight = true;
+    public int jumpPower = 1250;
+    public float moveX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        //controls
+        moveX = Input.GetAxis("Horizontal");
+        //animations
+        //direction
+        if (moveX < 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        else if(moveX > 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+        //physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump()
+    {
+        //jumping code
+    }
+
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+}
