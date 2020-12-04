35c79f727759ebd47936d09566f4f0cf5b443f30
riccardino <rikmatt@hotmail.it>
2020-12-04 20:48:07 +0100

    spawner

--- Kovid19Game/Assets/Scripts/EnemySpawnerScript.cs
+++ Kovid19Game/Assets/Scripts/EnemySpawnerScript.cs
@@ -6,6 +6,9 @@ public class EnemySpawnerScript : MonoBehaviour
 {
     public GameObject enemy;
     float randX;
+    float offset;
+    int cont = 0;
+    public int threshold = 5;
     Vector2 whereToSpawn;
     public float spawnRate = 2f;
     float nextSpawn = 0.0f;
@@ -18,13 +21,17 @@ public class EnemySpawnerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Time.time > nextSpawn)
+        if (cont < threshold)
         {
-            nextSpawn = Time.time + spawnRate;
-            randX = Random.Range(-22f, 70f);
-            whereToSpawn = new Vector2(randX, transform.position.y);
-            Instantiate(enemy, whereToSpawn, Quaternion.identity);
+            if (Time.time > nextSpawn)
+            {
+                nextSpawn = Time.time + spawnRate;
+                randX = Random.Range(-22f, 70f);
+                whereToSpawn = new Vector2(randX, transform.position.y);
+                Instantiate(enemy, whereToSpawn, Quaternion.identity);
+                cont++;
+                Debug.Log(cont);
+            }
         }
-        
     }
 }
