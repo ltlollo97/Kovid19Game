a879b4a45520f69e45ed2732d533184231421302
ltlollo97 <lorenzo.nardi0102@outlook.it>
2020-12-04 17:42:38 +0100

    updated anims

---
+++ Kovid19Game/Assets/Scripts/Boss.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boss : Enemy
+{
+    public AnimationClip explosion;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 400;
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+
+        if (Input.GetKeyDown(KeyCode.Q) == true)
+        {
+            Destroy(gameObject);
+            anim.Play(explosion.name);
+        }
+    }
+
+    public override void EnemyMove()
+    {
+        transform.position *= Time.deltaTime * 0.5f;
+    }
+}
+
