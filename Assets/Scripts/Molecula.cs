7096307283b1dca778dc5533f128626db3cd3307
ltlollo97 <lorenzo.nardi0102@outlook.it>
2020-12-06 18:09:22 +0100

    Reverted to Emmy's version

---
+++ Assets/Scripts/Molecula.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Molecula : Enemy
+{
+    public int xDirection;
+    public int yDirection;
+    private Vector3 startingPosition;
+    [SerializeField] Transform playerCharacter;
+    public float aggroRange = 6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        enemySpeed = 0.5f;
+        health = 200;
+        xDirection = RandomExcept(-1, 1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        float distToPlayer = Vector2.Distance(transform.position, playerCharacter.position);
+
+        if (distToPlayer < aggroRange)
+        {
+            //move towards player
+            ChasePlayer();
+        }
+        else
+        {
+            //move casually
+            EnemyMove();
+        }
+
+        if (health == 0)
+        {
+            Destroy(gameObject);
+
+            player.SetUltraCooldown(player.GetUltraCooldown() - 2f); //if enemy is killed, reduce player's ultra cooldown
+
+            Debug.Log(player.GetUltraCooldown());
+        }
+    }
+
+    public override void EnemyMove()
+    {
+        transform.position = new Vector3(moveX, moveY, 0f) + new Vector3(1,0,0) * Mathf.Sin(Time.realtimeSinceStartup) * xDirection;
+        
+    }
+
+    new public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Attack")
+        {
+            health -= 50;
+            Debug.Log("Hit");
+            SoundManagerScript.PlaySound("enemy1Hit");
+        }
+    }
+
+    protected void ChasePlayer()
+    {
+        if (transform.position.x < playerCharacter.position.x)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(enemySpeed, 0);
+            Vector2 localScale = gameObject.transform.localScale;
+            localScale.x *= -1;
+            transform.localScale = localScale;
+        }
+
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(enemySpeed, 0);
+            Vector2 localScale = gameObject.transform.localScale;
+            localScale.x *= 1;
+            transform.localScale = localScale;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Shredder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour
+{
+    // Destroys game object
+    private float waitTime = 2.0f;
+    private float timer = 0.0f;
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(tag == "Attack" && timer > waitTime)
+        {
+            //if the attack does not hit nothing in 3 seconds, it gets destroyed
+            Destroy(gameObject); 
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag != "Player")
+        {
+            Destroy(gameObject);
+        }
+
+        if(gameObject.tag == "HealthKit" && collision.collider.tag == "Player") //healthkit destroys when player hits it
+        {
+            Destroy(gameObject);
+        }
+            
+    }
+}
