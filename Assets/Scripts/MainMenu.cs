7e5f82e1b9c6430d5ab5788027c88b913bb6a99b
ltlollo97 <lorenzo.nardi0102@outlook.it>
2020-12-16 20:51:57 +0100

    Updated Menu graphics

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer musicMixer;
+    public AudioMixer effectsMixer;
+
     //Closes game whenever clicked
     public void ExitGame()
     {
@@ -14,5 +18,15 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicMixer.SetFloat("volume", volume);
+    }
+
+    public void SetEffectsVolume(float volume)
+    {
+        effectsMixer.SetFloat("volume", volume);
     }
 }
